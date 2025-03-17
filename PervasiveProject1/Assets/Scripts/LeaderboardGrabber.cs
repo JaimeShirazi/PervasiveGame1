86f7698cab3819d8d361cf7b816398bdc01287a6
StormCat <stormcatdev@gmail.com>
2025-03-17 18:58:56 +1100

    imported leaderboard key system

---
+++ PervasiveProject1/Assets/Scripts/LeaderboardGrabber.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class LeaderboardGrabber : MonoBehaviour
+{
+    private string publicLeaderboardKey = "0754162563be5dcdd35262c0e49c75c7a14bee22865b2154bbc8d33caaf3d7f1";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
